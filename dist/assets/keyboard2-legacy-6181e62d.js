94b4f8605fb76087d35ddcce0f164ab328c15b4c
Pranav <pranavkp.me@outlook.com>
2023-07-13 08:47:50 +0530

    updated the package for cli

--- dist/assets/keyboard2-legacy-6181e62d.js
+++ dist/assets/keyboard2-legacy-6181e62d.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-8338143c.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-ee80be94.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
--- dist/assets/md.transition-legacy-c19ad050.js
+++ dist/assets/md.transition-legacy-c19ad050.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-8338143c.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.l,i=e.k}],execute:function(){
+System.register(["./index-legacy-ee80be94.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.l,i=e.k}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
